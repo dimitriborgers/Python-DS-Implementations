ac72381b11f8d12d5696279c17d420c919564a9b
Dimitri Borgers <dimitri.borgers@gmail.com>
2018-10-11 12:05:02 -0400

    Create graph_traversals.py

---
+++ graph_traversals.py
@@ -0,0 +1,6 @@
+def DFS(g, u, discovered):
+    for e in g.incident edges(u): 
+        v = e.opposite(u)
+        if v not in discovered:
+            discovered[v] = e 
+            DFS(g, v, discovered)
