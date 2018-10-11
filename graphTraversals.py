5777dd79593e9375003c6da0f00e78b9ef7ba102
Dimitri Borgers <dimitri.borgers@gmail.com>
2018-10-11 11:54:07 -0400

    Update graphTraversals.py

--- graphTraversals.py
+++ graphTraversals.py
@@ -2,5 +2,5 @@ def DFS(g, u, discovered):
     for e in g.incident edges(u): 
 		v = e.opposite(u)
 		if v not in discovered:
-    		discovered[v] = e 
-    		DFS(g, v, discovered)
+            discovered[v] = e 
+    	    DFS(g, v, discovered)
