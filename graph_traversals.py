1d41b641f467b3b421f4c45ba7855130cb25b019
Dimitri Borgers <dimitri.borgers@gmail.com>
2018-10-11 14:20:56 -0400

    Update graph_traversals.py

--- graph_traversals.py
+++ graph_traversals.py
@@ -4,3 +4,15 @@ def DFS(g, u, discovered):
         if v not in discovered:
             discovered[v] = e 
             DFS(g, v, discovered)
+
+def BFS(g, s, discovered):
+    level = [s]
+    while len(level) > 0:
+        next level = [ ] 
+        for u in level:
+            for e in g.incident edges(u): 
+                v = e.opposite(u)
+                if v not in discovered:
+                    discovered[v] = e
+                    next level.append(v) 
+        level = next level
