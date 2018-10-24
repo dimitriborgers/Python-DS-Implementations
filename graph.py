7d129509b246ea643ff0a5ce62d3e06e08f33fef
Dimitri Borgers <dimitri.borgers@gmail.com>
2018-10-24 12:49:59 -0400

    Update graph.py

--- graph.py
+++ graph.py
@@ -1,4 +1,85 @@
-class Graph:
+class Graph1:
+    def __init__(self, directed=False):
+        self._outgoing = {}
+        self._incoming = {} if directed else self._outgoing
+
+    def is_directed(self):
+        return self._incoming is not self._outgoing
+
+    def vertex_count(self):
+        return len(self._outgoing)
+
+    def vertices(self):
+        return self._outgoing.keys()
+
+    def edge_count(self):
+        total = sum(len(self._outgoing[v]) for v in self._outgoing)
+        return total if self.is_directed() else total // 2
+
+    def edges(self):
+        result = set()
+        for secondary_map in self._outgoing.values():
+            result.update(secondary_map.values())
+
+    def get edge(self, u, v):
+        return self._outgoing[u].get(v)
+
+    def degree(self, v, outgoing=True):
+        adj = self._outgoing if outgoing else self._incoming
+        return len(adj[v])
+
+    def incident_edges(self, v, outgoing=True):
+        adj = self._outgoing if outgoing else self._incoming
+        for edge in adj[v].values():
+            yield edge
+
+    def insert_vertex(self, x=None):
+        v = self.Vertex(x)
+        self._outgoing[v] = {}
+        if self.is_directed():
+            self._incoming[v] = {}
+            return v
+
+    def insert_edge(self, u, v, x=None):
+        e = self.Edge(u, v, x)
+        self._outgoing[u][v] = e
+        self._incoming[v][u] = e
+
+class Vertex:
+    __slots__ = '_element'
+
+    def __init__(self, x):
+        self._element = x
+
+    def element(self):
+        return self._element
+
+    def __hash__(self):
+        return hash(id(self))
+
+class Edge:
+    __slots__ = '_origin', '_destination' , '_element'
+
+    def __init__(self, u, v, x):
+        self._origin = u
+        self._destination = v
+        self._element = x
+
+    def endpoints(self):
+        return (self._origin, self._destination)
+
+    def opposite(self, v):
+        return self._destination if v is self._origin else self._origin
+
+    def element(self):
+        return self._element
+
+    def __hash__(self):
+        return hash((self._origin, self._destination))
+
+#--------------------------------------------------
+
+class Graph2:
     def __init__(self, size):
         self.adjMatrix = []
         for i in range(size):
@@ -30,8 +111,6 @@ class Graph:
                 print('{:4}'.format(val)),
             print
             
-#--------------------------------------------------
-
 class Vertex:
     def __init__(self, vertex):
         self.name = vertex
@@ -60,9 +139,10 @@ class Vertex:
         
     def __repr__(self):
         return str(self.neighbors)
+    
+#--------------------------------------------------
 
-
-class Graph:
+class Graph3:
     def __init__(self):
         self.vertices = {}
     
@@ -113,8 +193,6 @@ class Graph:
 def graph(g):
     """ Function to print a graph as adjacency list and adjacency matrix. """
     return str(g.adjacencyList()) + '\n' + '\n' + str(g.adjacencyMatrix())
-
-###################################################################################
 
 a = Vertex('A')
 b = Vertex('B')
